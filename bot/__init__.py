8800e039413920ef31cb01e1749e511dc173de3c
pr13260 <67130996+pr13260@users.noreply.github.com>
2020-12-19 13:37:44 +0530

    Pvted

--- bot/__init__.py
+++ bot/__init__.py
@@ -64,14 +64,7 @@ START_OTHER_USERS_TEXT = get_config(
     (
         "Hi. ☺️\n"
         "Thank you for using me 😬\n\n"
-        "This is an Open Source Project available on "
-        "https://github.com/SpEcHIDe/NoPMsBot\n\n\n"
-        "If you are the owner of this bot, "
-        "and are seeing this message 🤦‍♂️, "
-        "means that you have not set up "
-        "the ENVironment variables properly "
-        "for the bot to function.\n\n\n"
-        "ℹ️ Subscribe @SpEcHlDe if you 😍 using this bot❗️❣️"
+        "ℹ️ Subscribe @united_hackerZ if you 😍 using this bot❗️❣️"
     )
 )
 # check online status of your bot
@@ -80,8 +73,7 @@ ONLINE_CHECK_START_TEXT = get_config(
     (
         "i am online <b>master</b>\n\n"
         "This is an Open Source Project available on "
-        "https://github.com/SpEcHiDe/NoPMsBot\n\n\n"
-        "ℹ️ Subscribe @SpEcHlDe if you 😍 using this bot❗️❣️"
+        "ℹ️ Subscribe @united_hackerZ if you 😍 using this bot❗️❣️"
     )
 )
 # IDEKWBYRW
